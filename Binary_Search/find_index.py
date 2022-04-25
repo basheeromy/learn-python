cbadd6008f9b950e3b74770ff571dde7c6a71287
Muhammed Basheer <basheerkomassery@gmail.com>
2022-04-25 20:19:01 +0530

    heading changed

--- Binary_Search/find_index.py
+++ Binary_Search/find_index.py
@@ -1,4 +1,4 @@
-# this program is written in binary search algorithm
+# this program is written in linear search algorithm
 Marks = [45,23,65,78,23,98,99,89,67,47]
 mark = int(input('enter the mark to find its position in list\n'))
 for i in range (len(Marks)):
