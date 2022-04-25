8068e272d5d6b751796d9429cde34087342f2214
Muhammed Basheer <basheerkomassery@gmail.com>
2022-04-25 17:23:50 +0530

    new file as sample for binary search algorithm

---
+++ Binary_Search/find_index.py
@@ -0,0 +1,9 @@
+# this program is written in binary search algorithm
+Marks = [45,23,65,78,23,98,99,89,67,47]
+mark = int(input('enter the mark to find its position in list\n'))
+for i in range (len(Marks)):
+    if Marks [i-1] == mark:
+        print (f'position of the given number in list is {i}')
+        break
+else:
+    print ('item not found')
