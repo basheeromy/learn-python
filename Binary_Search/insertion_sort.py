d48854fa7df6ff53e7c140c37310f97e50735f0e
Muhammed Basheer <basheerkomassery@gmail.com>
2022-04-26 23:42:50 +0530

    new set of code in insertion sort algorithm

---
+++ Binary_Search/insertion_sort.py
@@ -0,0 +1,14 @@
+# set of codes in insertion sort algorithm
+# sort an array 
+arr = [3,6,5,8,12,45,67,89,54,43]
+for i in range (1,len(arr)):
+    temp = arr[i]
+    j= i-1
+
+    while j >= 0 and temp < arr[j]:
+        arr[j+1] = arr[j]
+        j -= 1
+
+    arr[j+1] = temp
+
+print (arr)
